0de13f9e8b61958007e7c81e8ffb68417d31aadd
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-28 17:36:36 -0700

    all files setup, tables set up, middleware created, etc

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,19 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+
+DROP TABLE IF EXISTS users1 CASCADE;
+DROP TABLE IF EXISTS todos CASCADE;
+
+CREATE TABLE users1 (
+	id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  email VARCHAR NOT NULL UNIQUE,
+  password_hash VARCHAR NOT NULL
+);
+
+CREATE TABLE todos (
+		id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+		user_id BIGINT,
+		task_name VARCHAR NOT NULL,
+		completed BOOLEAN NOT NULL
+);
+
